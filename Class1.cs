6d36cb4f8109f2b9521bd7ab06966d4141265df0
akramwasimgithub <imwasim322@gmail.com>
2021-12-04 12:16:44 +0530

    class1 added

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace demo
+{
+    class Class1
+    {
+        public void add()
+        {
+            int a = 6;
+            int b = 7;
+            int c = a + b;
+                
+        }
+    }
+}
