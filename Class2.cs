d6f5cd3fb7468adfd2292c9bac937d51ee0d0348
akramwasimgithub <imwasim322@gmail.com>
2021-12-04 12:17:33 +0530

    class2 added

---
+++ Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace demo
+{
+    class Class2
+    {
+        public void sub()
+        {
+            int a = 6;
+            int b = 7;
+            int c = a - b;
+
+        }
+    }
+}
