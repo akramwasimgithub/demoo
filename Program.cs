1331b4b44de7fef2604dabe7c99a7df0366edf9d
akramwasimgithub <imwasim322@gmail.com>
2021-12-04 12:18:45 +0530

    main method's class added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 ob = new Class1();
+            Class2 on = new Class2();
+            ob.add();
+            on.sub();
+
+        }
+    }
+}
